2427380e60f93938308e4d8fae736141c037e8da
vinicius-prates <viniprates2211@gmail.com>
2023-01-25 10:48:18 -0300

    Added trpc and init front-end

--- index.ts
+++ index.ts
@@ -2,11 +2,45 @@ import express from "express";
 import cors from "cors";
 import { z } from "zod";
 import { prisma } from "./utils/prisma";
+import superjson from "superjson"
 import { isAuthenticated, isAuthenticatedRequest } from "./utils/middleware";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import { initTRPC, TRPCError } from "@trpc/server";
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const t = initTRPC.create({
+    transformer: superjson,
+    errorFormatter({ shape }) {
+        return shape;
+    },
+})
+
+const appRouter = t.router({
+    allSneaker: t.procedure.query(async () => {
+        try {   
+            const allSneakers = await prisma.sneaker.findMany()
+            return { allSneakers }
+        } catch (error) {
+            throw new TRPCError({
+                code: "INTERNAL_SERVER_ERROR",
+                cause: error,
+                message:"KKKKKKK deu merda"
+            })
+        }
+    }),
+})
+
+export type AppRouter = typeof appRouter 
+app.use(
+    '/trpc',
+    trpcExpress.createExpressMiddleware({
+        router: appRouter,
+    }),    
+);
+
 
 app.get("/", (req, res) => {
   return res.status(200).json({ message: "Home page" });
