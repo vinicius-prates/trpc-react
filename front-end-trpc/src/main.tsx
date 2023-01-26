aacd37847982501ceb0d30cf28987701c3a081c8
vinicius-prates <viniprates2211@gmail.com>
2023-01-26 13:41:28 -0300

    added react router

--- front-end-trpc/src/main.tsx
+++ front-end-trpc/src/main.tsx
@@ -2,11 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
  
 const queryClient = new QueryClient()
 
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>,
+    
+  }
+])
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <QueryClientProvider client={queryClient}>
-       <App />
+       <RouterProvider router={router}/>
      </QueryClientProvider>
 )
