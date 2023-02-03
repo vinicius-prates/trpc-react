96f2f43fa04140b5760a84909b7b201899b0b8eb
vinicius-prates <viniprates2211@gmail.com>
2023-02-03 10:28:12 -0300

    feat: verify user login

--- front-end-trpc/src/App.tsx
+++ front-end-trpc/src/App.tsx
@@ -1,17 +1,23 @@
 import { useQuery } from "@tanstack/react-query";
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./App.css";
 import { AllSneakers } from "./components/AllSneakers";
 import { api } from "./util/trpc";
 function App() {
+  const navi = useNavigate();
 
-  const meQuery = useQuery(['me'], () => {
-    return api.me.query()
-  });
-  
+  const meQuery = useQuery(["me"], () => api.me.query());
+   
+  useEffect(() => {
+    if (!meQuery.data?.user) {
+      navi("/login")
+    }
+  }, [])
+
   return (
     <div className="flex flex-col h-screen bg-[#fafafa]">
-     {JSON.stringify(meQuery.data)}
+      {JSON.stringify(meQuery.data)}
       <div className="flex flex-col items-center text-center m-10">
         <h1 className="text-2xl font-bold text-white"> All Sneakers </h1>
         <AllSneakers />
